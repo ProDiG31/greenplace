c94a171d4001e83e655b9489091f3114aa57c8b3
Unknown <rducros34@gmail.com>
2018-01-09 11:48:37 +0100

    Geocode Adding

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var http = require('http');
 var url = require('url');
+var googleMapsClient = require('@google/maps').createClient({
+	key: 'AIzaSyDrmAKRosfqSjSXvy2aLhZC7yyhVZ66JAs'
+});
 
 
 http.createServer(function (req, res) {
-    
+
 	var page = url.parse(req.url).pathname;
-	
+
 	console.log(page);
-	
+
 	res.writeHead(200, {
 		'content-type': 'text/plain'
 	});
-	
+
 	res.write('Hello world');
+
+	// Geocode an address.
+	googleMapsClient.geocode({
+		address: '1600 Amphitheatre Parkway, Mountain View, CA'
+	}, function (err, response) {
+		if (!err) {
+			console.log("GEOCODE OK !")
+			console.log(response.json.results);
+		}
+	});
+
 	res.end();
-	
+
 	console.log("node test")
 
 }).listen(3000);
