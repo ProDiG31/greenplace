a7775b759c50e6a88f7aa551f6178e0cce179e6d
Unknown <rducros34@gmail.com>
2018-01-09 11:38:45 +0100

    App.js ++

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function(req,res){
 
-	res.writeHead(200,{
+http.createServer(function (req, res) {
+    
+	var page = url.parse(req.url).pathname;
+	
+	console.log(page);
+	
+	res.writeHead(200, {
 		'content-type': 'text/plain'
 	});
+	
 	res.write('Hello world');
 	res.end();
+	
 	console.log("node test")
+
 }).listen(3000);
