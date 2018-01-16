e6f4c0f6a9d9a74f45b6bb5eca2009eecd76f1a9
Unknown <rducros34@gmail.com>
2018-01-16 15:11:15 +0100

    V0.27 - Mapping Coté Serveur OK

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const postman = require('postman');
-//const request = require('fetch.io');
+const file = require('./app/data/arbres-d-alignement.json');
+
 const app = express();
 //const googleMapsClient = require('@google/maps').createClient({
 //  key: 'AIzaSyDrmAKRosfqSjSXvy2aLhZC7yyhVZ66JAs'
@@ -31,60 +32,29 @@ app.get('/dataJson', (req, res) => {
 
 app.get('/dataLength', (res) => {
 	//	var json = res;
-	console.log(res.picture)
+	//	console.log(res.picture)
 	return count = Object.keys(res).length;
 })
 
 app.get('/dataMap', (req, res) => {
-	fs.readFile("./app/data/arbres-d-alignement.json", 'utf-8', (error, content) => {
-		var data = JSON.parse(content)
-		const arr = []
-
-		data.forEach((val) => {
-			var contentString = '<div id="content">' +
-				'<div id="siteNotice">' +
-				'</div>' +
-				'<h1 id="firstHeading" class="firstHeading">' + val.fields.patrimoine + '</h1>' +
-				'<div id="bodyContent">' +
-				'<p><b>Location : </b>' + val.fields.adresse + '</p>' +
-				'<p><b> Recorded : </b>' + val.record_timestamp + '</p>' +
-				'<p><b> Coordinate : </b>' + '[' + val.geometry.coordinates[1] + ',' + val.geometry.coordinates[0] + ']' + '</p>' +
-				'</div>' +
-				'</div>';
-
-			var infowindow = new google.maps.InfoWindow({
-				content: contentString,
-				position: {
-					lat: val.geometry.coordinates[1],
-					lng: val.geometry.coordinates[0]
+	arr = [];
+	for (var val in file) {
+		let coordinate = file[val].fields.geo_shape.coordinates;
+		for (let i = 0; i < coordinate.length; i++) {
+			flightPlanCoordinates = coordinate.map((obj) => {
+				let robj = {
+					lat: obj[1],
+					lng: obj[0]
 				}
-			});
-
-			var imageTree = "/data/deciduous-tree.png";
-
-			let coordinate = val.fields.geo_shape.coordinates;
-			let flightPlanCoordinates = new Array(coordinate.length);
-
-			for (let i = 0; i < coordinate.length; i++) {
-				flightPlanCoordinates[i] = {
-					lat: coordinate[i][1],
-					lng: coordinate[i][0]
-				};
-			}
-
-			var flightPath = new google.maps.Polyline({
-				path: flightPlanCoordinates,
-				geodesic: true,
-				strokeColor: '#73BA37',
-				strokeOpacity: .6,
-				strokeWeight: 6
-			});
-			arr.push(flightPath)
-		})
-//		console.log("Got response: " + data.picture);
-		return(arr)
-	})
-
+				return robj;
+			})
+		}
+		const arrTree = [file[val].fields.patrimoine, file[val].fields.adresse, file[val].record_timestamp, file[val].geometry.coordinates[1], file[val].geometry.coordinates[0], flightPlanCoordinates]
+		arr.push(arrTree);
+	}
+	console.log("Return go");
+	res.json(arr);
 })
+
 
 app.listen(3000);
